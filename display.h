bd2651df2e7e99c581610a8929cf7bf8f522f7e5
JackGrence <green1181772@gmail.com>
2018-04-19 11:23:04 +0800

    Add display module

---
+++ display.h
@@ -0,0 +1,17 @@
+#include <time.h>
+#include "linklist.h"
+
+#ifndef DISPLAY
+#define DISPLAY 
+#define DEBUG_PRINT 1
+
+clock_t begin;
+clock_t end;
+double spent;
+void print_ary (int *ary, int len);
+void print_tableList (table_list *tableL);
+void calc_time (const char *msg);
+void print_intList (int_list *intList);
+void print_rgbList (rgb_list *rgbList);
+
+#endif /* ifndef DISPLAY */

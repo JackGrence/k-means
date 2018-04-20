ce69f70c21a08ad322d0d2876425f0c770f0265d
JackGrence <green1181772@gmail.com>
2018-04-20 14:01:35 +0800

    Complete clustring and write to jpeg file

--- main.c
+++ main.c
@@ -17,35 +17,85 @@ struct my_error_mgr {
 typedef struct my_error_mgr * my_error_ptr;
 
 METHODDEF(void) my_error_exit (j_common_ptr cinfo);
-GLOBAL(int) read_JPEG_file (char * filename);
-rgb_list ** clustering(int k, rgb_list *rgbList, int rgbList_len);
+rgb_list *read_JPEG_file (char * filename);
+rgb_list **clustering(int k, rgb_list *rgbList, int rgbList_len);
 int get_distance_square (rgb_list *list1, rgb_list *list2);
+int get_target_pos(int k, rgb_list **cluster_ary, rgb_list *rgbList);
 bool clustering_with_clusterAry (int k, rgb_list **cluster_ary);
+void set_image_buffer (int k, rgb_list **cluster_ary, rgb_list *rgbList);
+GLOBAL(void) write_JPEG_file (char * filename, int quality, int k, rgb_list **cluster_ary, rgb_list *rgbList);
 
 JSAMPLE * image_buffer;	/* Points to large array of R,G,B-order data */
 int image_height;	/* Number of rows in image */
 int image_width;		/* Number of columns in image */
+int image_pixel_len;
 
 int
 main(int argc, char *argv[])
 {
-    char ary[] = {1, 2, 3, 4, 5, 6};
+    if (argc < 3)
+    {
+        printf ("Usage: ./kmeans file.jpg k\n");
+        return 1;
+    }
+    int i;
+    int k = strtol(argv[2], NULL, 10);
     rgb_list *rgbList;
     rgb_list **final_ary;
-    rgbList = create_rgbList ();
-    add_rgbList (rgbList, &ary[0]);
-    add_rgbList (rgbList, &ary[1]);
-    add_rgbList (rgbList, &ary[2]);
-    add_rgbList (rgbList, &ary[3]);
-    final_ary = clustering (2, rgbList->next, 4);
-    int i;
-    printf ("main\n");
-    print_rgbList (rgbList->next);
-    for (i = 0; i < 2; i++)
+    rgbList = read_JPEG_file (argv[1]);
+    final_ary = clustering (k, rgbList->next, image_pixel_len);
+
+    rgbList->next = reverse_rgbList (rgbList->next, NULL);
+
+    image_buffer = (unsigned char *) calloc (image_width * 3, sizeof (unsigned char));
+    argv[1][0] = argv[2][0];
+    write_JPEG_file (argv[1], 75, k, final_ary, rgbList->next);
+
+    for (i = 0; i < k; i++)
     {
-        print_rgbList (final_ary[i]);
+        free_rgbList (final_ary[i]);
     }
+    free (final_ary);
+    free_rgbList (rgbList);
+    free (image_buffer);
     return 0;
+}
+
+void
+set_image_buffer (int k, rgb_list **cluster_ary, rgb_list *rgbList)
+{
+    int pixel_pos;
+    int target;
+    rgb_list *cur_rgbList;
+    image_buffer = (unsigned char *) calloc (image_pixel_len * 3, sizeof (unsigned char));
+    // scan every pixel
+    for (pixel_pos = image_pixel_len - 1, cur_rgbList = rgbList; cur_rgbList != NULL; pixel_pos--, cur_rgbList = cur_rgbList->next)
+    {
+        target = get_target_pos (k, cluster_ary, cur_rgbList);
+        memcpy (&image_buffer[pixel_pos * 3], cluster_ary[target], 3);
+    }
+}
+
+int
+get_target_pos(int k, rgb_list **cluster_ary, rgb_list *rgbList)
+{
+    int i;
+    int min;
+    int distance;
+    int target;
+
+    target = 0;
+    min = get_distance_square (cluster_ary[0], rgbList);
+    for (i = 1; i < k; i++)
+    {
+        distance = get_distance_square (cluster_ary[i], rgbList);
+        if (distance < min)
+        {
+            min = distance;
+            target = i;
+        }
+    }
+    return target;
 }
 
 rgb_list **
@@ -96,7 +146,7 @@ clustering(int k, rgb_list *rgbList, int rgbList_len)
                 min = distance;
             }
         }
-        add_rgbList (target_list, (char *) cur_rgbList);
+        add_rgbList (target_list, (unsigned char *) cur_rgbList);
     }
 
     while (clustering_with_clusterAry (k, cluster_ary));
@@ -109,6 +159,7 @@ clustering_with_clusterAry (int k, rgb_list **cluster_ary)
 {
     int i;
     int list_len;
+    int RGBsum[3];
     rgb_list *cur_rgbList;
     rgb_list **scanList_ary;
     bool result;
@@ -120,18 +171,24 @@ clustering_with_clusterAry (int k, rgb_list **cluster_ary)
     for (i = 0; i < k; i++)
     {
         scanList_ary[i] = cluster_ary[i]->next;
-        memset (cluster_ary[i], 0, 3);
+        memset (&RGBsum, 0, 3 * sizeof (int));
         list_len = 0;
         for (cur_rgbList = cluster_ary[i]->next; cur_rgbList != NULL; cur_rgbList = cur_rgbList->next)
         {
-            cluster_ary[i]->r += cur_rgbList->r;
-            cluster_ary[i]->g += cur_rgbList->g;
-            cluster_ary[i]->b += cur_rgbList->b;
+            RGBsum[0] += cur_rgbList->r;
+            RGBsum[1] += cur_rgbList->g;
+            RGBsum[2] += cur_rgbList->b;
             list_len++;
         }
-        cluster_ary[i]->r /= list_len;
-        cluster_ary[i]->g /= list_len;
-        cluster_ary[i]->b /= list_len;
+        if (list_len != 0)
+        {
+            RGBsum[0] /= list_len;
+            RGBsum[1] /= list_len;
+            RGBsum[2] /= list_len;
+            cluster_ary[i]->r = RGBsum[0];
+            cluster_ary[i]->g = RGBsum[1];
+            cluster_ary[i]->b = RGBsum[2];
+        }
     }
 
     int min;
@@ -178,7 +235,7 @@ clustering_with_clusterAry (int k, rgb_list **cluster_ary)
 
             if (target_list != cluster_ary[scan])
             {
-                add_rgbList (target_list, (char *) cur_rgbList);
+                add_rgbList (target_list, (unsigned char *) cur_rgbList);
 
                 // remove current element from cur list
                 prev_list->next = cur_rgbList->next;
@@ -198,19 +255,19 @@ get_distance_square (rgb_list *list1, rgb_list *list2)
 {
     int sum;
     int diff;
-    diff = list1->r - list2->r;
+    diff = (int) list1->r - (int) list2->r;
     diff = abs (diff);
     sum = diff * diff;
-    diff = list1->g - list2->g;
+    diff = (int) list1->g - (int) list2->g;
     diff = abs (diff);
     sum += diff * diff;
-    diff = list1->b - list2->b;
+    diff = (int) list1->b - (int) list2->b;
     diff = abs (diff);
     sum += diff * diff;
     return sum;
 }
 
-GLOBAL(int)
+rgb_list *
 read_JPEG_file (char * filename)
 {
     struct jpeg_decompress_struct cinfo;
@@ -253,14 +310,20 @@ read_JPEG_file (char * filename)
     /* Step 6: while (scan lines remain to be read) */
     /*           jpeg_read_scanlines(...); */
 
+    image_pixel_len = 0;
+    image_width = row_stride / 3;
+    image_height = 0;
+    rgb_list *rgbList;
+    rgbList = create_rgbList ();
     while (cinfo.output_scanline < cinfo.output_height) {
         (void) jpeg_read_scanlines(&cinfo, buffer, 1);
         int i;
-        for (i = 0; i < row_stride; i++)
+        for (i = 0; i < row_stride; i += 3)
         {
-            printf("%d ", buffer[0][i]);
+            add_rgbList (rgbList, (unsigned char *) &buffer[0][i]);
+            image_pixel_len++;
         }
-        printf ("\n");
+        image_height++;
     }
 
     /* Step 7: Finish decompression */
@@ -273,7 +336,7 @@ read_JPEG_file (char * filename)
 
     fclose(infile);
 
-    return 1;
+    return rgbList;
 }
 
 /*
@@ -292,4 +355,62 @@ my_error_exit (j_common_ptr cinfo)
 
     /* Return control to the setjmp point */
     longjmp(myerr->setjmp_buffer, 1);
+}
+
+GLOBAL(void)
+write_JPEG_file (char * filename, int quality, int k, rgb_list **cluster_ary, rgb_list *rgbList)
+{
+  struct jpeg_compress_struct cinfo;
+  struct jpeg_error_mgr jerr;
+  FILE * outfile;		/* target file */
+  JSAMPROW row_pointer[1];	/* pointer to JSAMPLE row[s] */
+  int row_stride;		/* physical row width in image buffer */
+
+  /* Step 1: allocate and initialize JPEG compression object */
+  cinfo.err = jpeg_std_error(&jerr);
+  jpeg_create_compress(&cinfo);
+
+  /* Step 2: specify data destination (eg, a file) */
+  if ((outfile = fopen(filename, "wb")) == NULL) {
+    fprintf(stderr, "can't open %s\n", filename);
+    exit(1);
+  }
+  jpeg_stdio_dest(&cinfo, outfile);
+
+  /* Step 3: set parameters for compression */
+  cinfo.image_width = image_width; 	/* image width and height, in pixels */
+  cinfo.image_height = image_height;
+  cinfo.input_components = 3;		/* # of color components per pixel */
+  cinfo.in_color_space = JCS_RGB; 	/* colorspace of input image */
+  jpeg_set_defaults(&cinfo);
+  jpeg_set_quality(&cinfo, quality, TRUE /* limit to baseline-JPEG values */);
+
+  /* Step 4: Start compressor */
+  jpeg_start_compress(&cinfo, TRUE);
+
+  /* Step 5: while (scan lines remain to be written) */
+  /*           jpeg_write_scanlines(...); */
+
+  row_stride = image_width * 3;	/* JSAMPLEs per row in image_buffer */
+
+  int i;
+  int target;
+  while (cinfo.next_scanline < cinfo.image_height) {
+    for (i = 0; i < image_width; i++)
+    {
+      target = get_target_pos (k, cluster_ary, rgbList);
+      memcpy (&image_buffer[i * 3], cluster_ary[target], 3);
+      rgbList = rgbList->next;
+    }
+    row_pointer[0] = image_buffer;
+    (void) jpeg_write_scanlines(&cinfo, row_pointer, 1);
+  }
+
+  /* Step 6: Finish compression */
+
+  jpeg_finish_compress(&cinfo);
+  fclose(outfile);
+
+  /* Step 7: release JPEG compression object */
+  jpeg_destroy_compress(&cinfo);
 }
